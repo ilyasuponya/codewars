5dfb17e023428a7ec050f8ad43e019f7c81b5310
Ilya Suponya <isoup363@gmail.com>
2023-05-05 15:31:12 +0300

    return negative

---
+++ 8-kyu/Return Negative.js
@@ -0,0 +1,17 @@
+///Check if the given number is positive using an if statement.
+///If it's positive, multiply it by -1 to make it negative.
+///If it's already negative, leave it as is.
+
+
+
+function makeNegative(num) {
+    if (num > 0) {
+      return -1 * num;
+    } else {
+      return num;
+    }
+  }
+
+///In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
